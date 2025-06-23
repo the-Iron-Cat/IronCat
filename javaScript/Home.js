6d845c89734f6f958ff39a423f1d15bf0a60c40f
dreadPaiertDavincci <dreadpaiertdavinci@gmail.com>
2025-06-23 22:47:11 +0300

    asd

--- javaScript/Home.js
+++ javaScript/Home.js
@@ -1,17 +1,13 @@
   let started = false;
-
   function startCounting() {
     const boxes = document.querySelectorAll('.Box');
-
     boxes.forEach(box => {
       const h3 = box.querySelector('h3');
       const span = h3.querySelector('span');
       const text = h3.childNodes[0].nodeValue.replace(/"/g, "").trim(); // إزالة علامتي التنصيص
       const target = parseInt(text);
       let count = 0;
-
       const increment = target / 200;
-
       function updateCounter() {
         count += increment;
         if (count < target) {
@@ -21,11 +17,9 @@
           h3.childNodes[0].nodeValue = `"${target}" `;
         }
       }
-
       updateCounter();
     });
   }
-
   window.addEventListener("scroll", () => {
     const section = document.querySelector(".completion-Continar");
     const sectionTop = section.getBoundingClientRect().top;
