a3cd59929f9e0d757d7acfff12a4deff6305aed0
dreadPaiertDavincci <dreadpaiertdavinci@gmail.com>
2025-07-16 19:24:21 +0300

    asdasdasd

--- javaScript/Univer.js
+++ javaScript/Univer.js
@@ -4,96 +4,96 @@ const universities = [
     country: "istanbul",
     name: "Altınbaş",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_53_image.webp",
-    logoImage: "./Images/Images2/imgi_54_image.webp"
+    backgroundImage: "../Images/Images2/imgi_53_image.webp",
+    logoImage: "../Images/Images2/imgi_54_image.webp"
   },
   {
     id: 2,
     country: "Ankara",
     name: "Ankara Medipol",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_20_image.webp",
-    logoImage: "./Images/Images2/imgi_21_image.webp"
+    backgroundImage: "../Images/Images2/imgi_20_image.webp",
+    logoImage: "../Images/Images2/imgi_21_image.webp"
   },
   {
     id: 3,
     country: "Ankara",
     name: "Atılım",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_25_image.webp",
-    logoImage: "./Images/Images2/imgi_26_image.png"
+    backgroundImage: "../Images/Images2/imgi_25_image.webp",
+    logoImage: "../Images/Images2/imgi_26_image.png"
   },
   {
     id: 4,
     country: "istanbul",
     name: "Bahçeşehir",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_27_image.webp",
-    logoImage: "./Images/Images2/imgi_28_image.webp"
+    backgroundImage: "../Images/Images2/imgi_27_image.webp",
+    logoImage: "../Images/Images2/imgi_28_image.webp"
   },
   {
     id: 5,
     country: "Ankara",
     name: "Başkent",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_33_image.webp",
-    logoImage: "./Images/Images2/imgi_34_image.webp"
+    backgroundImage: "../Images/Images2/imgi_33_image.webp",
+    logoImage: "../Images/Images2/imgi_34_image.webp"
   },
   {
     id: 6,
     country: "istanbul",
     name: "Beykent",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_29_image.webp",
-    logoImage: "./Images/Images2/imgi_30_image.webp"
+    backgroundImage: "../Images/Images2/imgi_29_image.webp",
+    logoImage: "../Images/Images2/imgi_30_image.webp"
   },
   {
     id: 7,
     country: "istanbul",
     name: "Beykoz",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_31_image.webp",
-    logoImage: "./Images/Images2/imgi_32_image.webp"
+    backgroundImage: "../Images/Images2/imgi_31_image.webp",
+    logoImage: "../Images/Images2/imgi_32_image.webp"
   },
   {
     id: 8,
     country: "Gaziantep",
     name: "Hasan Kalyoncu",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_47_image.webp",
-    logoImage: "./Images/Images2/imgi_48_image.webp"
+    backgroundImage: "../Images/Images2/imgi_47_image.webp",
+    logoImage: "../Images/Images2/imgi_48_image.webp"
   },
   {
     id: 9,
     country: "İzmir",
     name: "Yaşar",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_109_image.webp",
-    logoImage: "./Images/Images2/imgi_110_image.webp"
+    backgroundImage: "../Images/Images2/imgi_109_image.webp",
+    logoImage: "../Images/Images2/imgi_110_image.webp"
   },
   {
     id: 10,
     country: "Bursa",
     name: "Mudanya",
     link: "Read more",
-    backgroundImage: "./Images/Images2/imgi_71_image.webp",
-    logoImage: "./Images/Images2/imgi_72_image.webp"
+    backgroundImage: "../Images/Images2/imgi_71_image.webp",
+    logoImage: "../Images/Images2/imgi_72_image.webp"
   },
   {
     id: 11,
     country: "Kocaeli",
     name: "Kocaeli Sağlık ve Teknoloji",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_63_image.webp",
-    logoImage: "./Images/Images2/imgi_64_image.webp"
+    backgroundImage: "../Images/Images2/imgi_63_image.webp",
+    logoImage: "../Images/Images2/imgi_64_image.webp"
   },
   {
     id: 12,
     country: "Mersin",
     name: "Toros",
     link: "Read More",
-    backgroundImage: "./Images/Images2/imgi_103_image.webp",
-    logoImage: "./Images/Images2/imgi_104_image.webp"
+    backgroundImage: "../Images/Images2/imgi_103_image.webp",
+    logoImage: "../Images/Images2/imgi_104_image.webp"
   }
 ];
 
@@ -127,10 +127,10 @@ function RenderCard(filter) {
 filterButtons.forEach(btn => {
     btn.addEventListener("click" ,() => {
      filterButtons.forEach(btn => btn.classList.remove("active"));
-    button.classList.add("active");
-    const selectedFilter = button.getAttribute("data-filter");
+    btn.classList.add("active");
+    const selectedFilter = btn.getAttribute("data-filter");
         RenderCard(selectedFilter);
-  }
+   }
   )
 }); 
 
